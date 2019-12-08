2a9aba1cc374a4f954bb96a3807499ed31a637ea
g-crow <genevieve.crow@gmail.com>
2019-12-08 15:15:48 -0700

    Update config

--- src/config.js
+++ src/config.js
@@ -2,17 +2,17 @@ const dev = {
   STRIPE_KEY: "pk_test_bkilKUbMjA4atuJiJJVhO0NB00QOQILxtX",
   s3: {
     REGION: "us-east-2",
-    BUCKET: "notes-app-2-api-dev-attachmentsbucket-ppdpr6z8sbc4"
+    BUCKET: "climbs-app-api-dev-serverlessdeploymentbucket-g2gqakv2up2"
   },
   apiGateway: {
     REGION: "us-east-2",
-    URL: "https://vmlh3od7m5.execute-api.us-east-2.amazonaws.com/dev"
+    URL: "https://ah9guh2gqh.execute-api.us-east-2.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-2",
-    USER_POOL_ID: "us-east-2_dRWW7oBaG",
-    APP_CLIENT_ID: "6c24sqeg58mnlqsrvlmabqfatp",
-    IDENTITY_POOL_ID: "us-east-2:7ea3d7c0-805c-4315-aefa-8c2e8fa82ecf"
+    USER_POOL_ID: "us-east-2_2i03sIrqA",
+    APP_CLIENT_ID: "urmokuer3dvj8ib5ofuksinoj",
+    IDENTITY_POOL_ID: "us-east-2:fc968b25-949e-487b-9737-e1fe51b9eb40"
   }
 };
 
@@ -20,17 +20,17 @@ const prod = {
   STRIPE_KEY: "pk_test_bkilKUbMjA4atuJiJJVhO0NB00QOQILxtX",
   s3: {
     REGION: "us-east-2",
-    BUCKET: "notes-app-2-api-prod-attachmentsbucket-vlc2k8xuqvgd"
+    BUCKET: "climbs-app-api-prod-serverlessdeploymentbucket-pwkk9nd7pmje"
   },
   apiGateway: {
     REGION: "us-east-2",
-    URL: "https://4e3olesiih.execute-api.us-east-2.amazonaws.com/prod"
+    URL: "https://4m9akrmuag.execute-api.us-east-2.amazonaws.com/prod"
   },
   cognito: {
     REGION: "us-east-2",
-    USER_POOL_ID: "us-east-2_ypJN0B4uW",
-    APP_CLIENT_ID: "7vt89o1m1dgd0fuuf8at5q9nfr",
-    IDENTITY_POOL_ID: "us-east-2:c1ba1b24-195f-4ae8-9438-ff4eef951670"
+    USER_POOL_ID: "us-east-2_kcfvqX0FB",
+    APP_CLIENT_ID: "mj398m0mel5pr681s4n903dlk",
+    IDENTITY_POOL_ID: "us-east-2:c1fc1286-3d9e-4593-b0cd-5ffdbc0505b3"
   }
 };
 
