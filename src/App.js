3860e6d8dfba100a1f2529e3a85799327ac53749
g-crow <genevieve.crow@gmail.com>
2019-12-08 17:03:58 -0700

    Rename Scratch>Git Tough

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App(props) {
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand>
-              <Link to="/">Scratch</Link>
+              <Link to="/">Git Tough</Link>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
